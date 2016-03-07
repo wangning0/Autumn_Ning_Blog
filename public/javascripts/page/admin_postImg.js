f070111a31fbdc0346a9edac9adf7627c4fdb7f1
wangning0 <172234437@qq.com>
2016-03-07 12:47:43 +0800

    图片上传功能

--- public/javascripts/page/admin_postImg.js
+++ public/javascripts/page/admin_postImg.js
@@ -1,6 +1,48 @@
 (function(){
-	
+	var uploadFile = getElementsByClass('uploadFile')[0];
+	var uploadImg = getElementsByClass('uploadImg')[0];
+	var form = document.getElementById('myForm');
+	var newWindow = document.getElementById('newWindow');
+	var body = document.body;
 
-
-		
-})
+	eventListener(uploadFile,'change',function(e){
+		form.submit();
+		eventListener(newWindow,'load',function(){
+			var h = newWindow.contentWindow.document.getElementsByTagName('pre')[0].innerHTML;
+			h = JSON.parse(h);
+			if( h.status == 0 ){
+				body.removeChild(newWindow);
+				var newWindowl = document.createElement('iframe');
+				newWindowl.setAttribute('name','newWindow');
+				newWindowl.setAttribute('id','newWindow');
+				newWindowl.style.display = 'none';
+				body.appendChild(newWindowl);
+			}
+		})
+	})
+	function eventListener(obj,eName,callback){
+		if( obj.addEventListener ) {
+			obj.addEventListener(eName,callback);
+		} else if( obj.attachEvent ){
+			obj.attachEvent('on'+eName,callback);
+		}
+	}
+	function getElementsByClass(className,tagName){
+		if( document.getElementsByClassName ){
+			return document.getElementsByClassName(className);
+		} else {
+			tagName = tagName || '*';
+			var tag = document.getElementsByTagName(tagName);
+			var result = [];
+			for( var i = 0;i<tag.length;i++ ){
+				var n = tag[i].className.split(' ');
+				for( var j=0;j<n.length;j++ ){
+					if( n[j] == className ){
+						result.push(tag[i]);
+					}
+				} 
+			}
+			return result;
+		}
+	}
+})()
--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,72 @@
 var express = require('express');
+var formidable = require('formidable');
+var fs = require('fs');
+
 var router = express.Router();
 
 router.get('/login',function(req,res,next){
 	res.render('admin/login');
-})
+});
 router.get('/postImg',function(req,res,next){
 	res.render('admin/postImg');
-})
+});
 router.get('/postArticle',function(req,res,next){
 	res.render('admin/postArticle');
-})
+});
 router.get('/blogs',function(req,res,next){
 	res.render('admin/blogs');
+});
+
+
+router.post('/uploadImg',function(req,res,next){
+	var form = new formidable.IncomingForm();
+	form.uploadDir = 'public/avator/';
+	form.keepExtensions = true;
+	form.maxFieldsSize = 10 *1024 * 1024;
+
+	form.parse(req,function(err,fields,files){
+		if( err ){
+			res.render('error',{error:err});
+			return false;
+		} 
+
+		var extName = '';	//后缀名
+		console.log('222',files.file.type);
+		switch (files.file.type) {
+			case 'image/pjpeg':
+				extName = 'jpg';
+				break;
+			case 'image/jpeg':
+				extName = 'jpg';
+				break;
+			case 'image/png':
+				extName = 'png';
+				break;
+			case 'image/x-png':
+				extName = 'png';
+				break;
+			case 'image/gif':
+				extName = 'gif';
+		}
+		if( extName.length == 0 ){
+			res.render('error',{error:'不支持该图片格式'});
+			return;
+		} 
+		var avatarName = files.file.name;
+		var newPath = form.uploadDir + avatarName;
+
+		fs.renameSync(files.file.path,newPath);
+		res.send({
+			status:0,
+			msg:'上传成功',
+			body:'avator/' + avatarName
+		});
+	})
 })
 module.exports = router;
+
+
+
+
+
+
